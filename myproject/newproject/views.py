607ce02c1dbca6b17ebcd8437b5f324a23d079b7
saisrk <i.am.songoku@gmail.com>
2014-07-19 23:22:01 +0530

    Update views.py
    Added functionality views.

--- myproject/newproject/views.py
+++ myproject/newproject/views.py
@@ -8,3 +8,15 @@ def archive(request):
     temp = loader.get_template('archive.html')
     cont = Context({'posts': posts})
     return HttpResponse(temp.render(cont))
+
+def blog_home(request):
+    latest_posts = BlogPost.objects.order_by('-timestamp')[:5]
+    temp = loader.get_template('bloghome.html')
+    cont = Context({"latest_posts": latest_posts})
+    return HttpResponse(temp.render(cont))
+
+def get_article(request, id):
+    post = BlogPost.objects.get(pk=id)
+    temp = loader.get_template("article.html")
+    cont = Context({'post': post})
+    return HttpResponse(temp.render(cont))
