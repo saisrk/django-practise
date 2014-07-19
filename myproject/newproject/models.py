057b7efc4da281239a0a97d9e409613d0e4d98a3
saisrk <i.am.songoku@gmail.com>
2014-07-19 23:21:17 +0530

    Update models.py
    Added Metadata to order by timestamp

--- myproject/newproject/models.py
+++ myproject/newproject/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.contrib import admin
 
+
 class BlogPost(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
     timestamp = models.DateTimeField()
 
+    class Meta:
+        ordering = ('-timestamp',)
+
     def __unicode__(self):
         return self.title
 
+
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'timestamp')
+
